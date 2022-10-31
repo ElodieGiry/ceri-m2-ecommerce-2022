52435c4168434b09cb07df0386f86141f7c4818d
BERTHORELLY Lea <leabty.lb@gmail.com>
2022-10-31 14:33:37 +0100

    Correction tests unitaires

--- test_main.py
+++ test_main.py
@@ -9,8 +9,6 @@ def test_get_album_by_artist():
     assert type("{nom_artiste}") == str
     assert response.status_code == 200
     assert response.json() == {"Artiste": "{nom_artiste}", 'Albums': getAlbumByArtist("{nom_artiste}")}
-    # assert type(response.json()) == dict
-    # assert type(getAlbumByArtist("{nom_artiste}")) == list
 
 def test_get_musics_by_artist():
     response = client.get("/{nom_artiste}/{nom_album}")
@@ -18,5 +16,8 @@ def test_get_musics_by_artist():
     assert type("{nom_album}") == str
     assert response.status_code == 200
     assert response.json() == {"Artiste": "{nom_artiste}", 'Musiques': getMusicsByArtist("{nom_artiste}", "{nom_album}")}
-    # assert type(response.json()) == dict
-    # assert type(getAlbumByArtist("{nom_artiste}")) == list
+
+def test_get_everything():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"Item": getEverything()}
