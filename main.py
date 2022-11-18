ceb1fb218c1fd4215282c34060ed00a0878dcd52
BERTHORELLY Lea <leabty.lb@gmail.com>
2022-11-18 09:27:09 +0100

    Retour du prix d'un album + ajout route /artistes

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
-from typing import Union
-
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-import sqlite3
 import numpy as np
-import psycopg2
 import mysql.connector
 
-DATABASE_USERNAME=""
-DATABASE_PASSWORD=""
-DATABASE_HOST=""
-DATABASE_PORT=""
-DATABASE_NAME=""
+DATABASE_USERNAME="ceri-commerce"
+DATABASE_PASSWORD="Elodie"
+DATABASE_HOST="localhost"
+DATABASE_PORT="3306"
+DATABASE_NAME="Vinyles"
 
 
-# connection = psycopg2.connect("dbname=Ecommerce user=ceri-commerce password=Elodie host=34.77.160.150 port=5432")
-# connection = psycopg2.connect("dbname=Vinyles user=ceri-commerce password=Elodie host=34.77.160.150 port=5432")
-connection = mysql.connector.connect(user='ceri-commerce', password='Elodie', host='localhost', database='Vinyles', port=3306)
+connection = mysql.connector.connect(user=DATABASE_USERNAME, password=DATABASE_PASSWORD, database=DATABASE_NAME)
 cursorDatabase = connection.cursor()
 
 
@@ -27,7 +21,7 @@ cursorDatabase.execute(query)
 nbAlbums=0
 for row in cursorDatabase:
     nbAlbums+=row[0]
-print(nbAlbums)
+# print(nbAlbums)
 
 def getEverything():
     cursorDatabase = connection.cursor()
@@ -62,7 +56,7 @@ def getArtists():
         AllArtists.append(row[0])
         
     cursorDatabase.close()
-    print(AllArtists[0])
+    # print(AllArtists[0])
     return AllArtists
 
 
@@ -78,11 +72,14 @@ def getAlbumByArtist(nomArtiste):
         Albums.append(row[1])
         Albums.append(row[2])
         Albums.append(row[3])
-    ListeAlbums=np.zeros((int(len(Albums)/4),4), dtype=object)
-    for i in range(0,int(len(Albums)/4)):
-        for j in range(0,int(len(Albums)/4)+3):
-            ListeAlbums[i,j]=Albums[(i*4)+j]
     cursorDatabase.close()
+
+    ListeAlbums=np.zeros((int(len(Albums)/4),4), dtype=object)
+    # print(int(len(Albums)/4))
+    for i in range(0,int(len(Albums)/4)):
+        for j in range(0,int(len(Albums)/4)+2):
+            ListeAlbums[i,j]=Albums[(i*4)+j]
+
     return ListeAlbums.tolist()
 
 
@@ -90,7 +87,7 @@ def getAlbumByArtist(nomArtiste):
 def getMusicsByArtist(nomArtiste, nomAlbum):
     cursorDatabase = connection.cursor()
     Musiques=[]
-    query=f'SELECT nomChanson, imageAlbum FROM chanson NATURAL JOIN album NATURAL JOIN artiste WHERE LOWER(nomArtiste) = LOWER(\'{nomArtiste}\') AND LOWER(nomAlbum) = LOWER(\'{nomAlbum}\') ORDER BY nomChanson'
+    query=f'SELECT nomChanson FROM chanson NATURAL JOIN album NATURAL JOIN artiste WHERE LOWER(nomArtiste) = LOWER(\'{nomArtiste}\') AND LOWER(nomAlbum) = LOWER(\'{nomAlbum}\') ORDER BY nomChanson'
     cursorDatabase.execute(query)
     for row in cursorDatabase:
         Musiques.append(row[0])
@@ -106,6 +103,15 @@ def getAlbumImage(nomArtiste, nomAlbum):
         imageAlbum=row[0]
     cursorDatabase.close()
     return imageAlbum
+
+def getAlbumPrice(nomArtiste, nomAlbum):
+    cursorDatabase = connection.cursor()
+    query=f'SELECT prixAlbum FROM album NATURAL JOIN artiste WHERE LOWER(nomArtiste) = LOWER(\'{nomArtiste}\') AND LOWER(nomAlbum) = LOWER(\'{nomAlbum}\')'
+    cursorDatabase.execute(query)
+    for row in cursorDatabase:
+        prixAlbum=row[0]
+    cursorDatabase.close()
+    return prixAlbum
 
 
 app = FastAPI()
@@ -129,7 +135,9 @@ app.add_middleware(
 def read_root():
     return {"Item": getEverything()}
     
-
+@app.get("/artistes")
+def read_item():
+    return {"Artistes": getArtists()}
 
 @app.get("/{nom_artiste}")
 def read_item(nom_artiste: str):
@@ -137,12 +145,7 @@ def read_item(nom_artiste: str):
     return {"Artiste": nom_artiste, 'Albums': getAlbumByArtist(nom_artiste)}
 
 
-@app.get("/artistes")
-def read_item():
-    return {"Artistes": getArtists()}
-
-
 @app.get("/{nom_artiste}/{nom_album}")
 def read_item(nom_artiste: str, nom_album: str):
     print("---------------------------------------------------------------")
-    return {"Artiste": nom_artiste, 'Musiques': getMusicsByArtist(nom_artiste,nom_album), 'Image': getAlbumImage(nom_artiste,nom_album)}
+    return {"Artiste": nom_artiste, 'Musiques': getMusicsByArtist(nom_artiste,nom_album), 'Image': getAlbumImage(nom_artiste,nom_album), 'Prix': getAlbumPrice(nom_artiste,nom_album)}    
