ec03ba71a7c42664e036594f3143bf76c498437f
BERTHORELLY Lea <73932490+Lea-BERTHORELLY@users.noreply.github.com>
2023-01-09 16:37:10 +0100

    Update database.py

--- back/database.py
+++ back/database.py
@@ -4,7 +4,6 @@ from os import environ
 USER=environ.get("USER")
 PASSWORD=environ.get("PASSWORD")
 DBNAME=environ.get("DBNAME")
-HOST=environ.get("HOST")
 MYSQL_PORT=3306
 
-connection = mariadb.connect(user=USER, password=PASSWORD, database=DBNAME, host=HOST, port=int(MYSQL_PORT))
+connection = mariadb.connect(user=USER, password=PASSWORD, database=DBNAME, unix_socket= "/cloudsql/ceri-m1-ecommerce-2022:europe-west1:mysql-primary", port=int(MYSQL_PORT))
